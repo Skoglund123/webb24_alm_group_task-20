16ce1185470ffb3631118cfd5f93c5547b6f042c
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-05-23 14:13:51 +0200

    add test setup functions

--- __tests__/test-setup.js
+++ __tests__/test-setup.js
@@ -1,15 +1,19 @@
 // test-setup.js
 process.env.NODE_ENV = "test";
-const sequelize = require("../src/config/database");
-const User = require("../src/models/User");
-// TODO: Add Accomodation model
+const { sequelize } = require("../src/config/database");
+const User = require("../src/models/userModel");
+const Accommodation = require("../src/models/accommodationModel");
 
 beforeAll(async () => {
-  await sequelize.sync();
+  await sequelize.sync({ force: true });
+});
+
+afterEach(async () => {
+  await Accommodation.destroy({ where: {} });
 });
 
 afterAll(async () => {
   await sequelize.close();
 });
 
-module.exports = { sequelize, User };
+module.exports = { sequelize, User, Accommodation };
