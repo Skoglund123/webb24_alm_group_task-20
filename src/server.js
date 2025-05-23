55a7819b8fd2f3e7b309eb0c6812ff7dc8ed6556
Moreal-code <mohamed.dukale@yh.nackademin.se>
2025-05-23 14:15:08 +0200

    remove unwanted code

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const User = require("./models/User");
 const Accommodation = require("./models/Accommodation");
 const UserRouter = require("./routes/userRoutes");
 const AccommodationRouter = require("./routes/accommodationRoutes");
-const testConnection = require("./config/database")
+const testConnection = require("./config/database");
 
 testConnection();
 
@@ -14,17 +14,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 
-User.hasMany(Accommodation, {
-  foreignKey: "userId",
-  onDelete: "CASCADE",
-});
-Accommodation.belongsTo(User, {
-  foreignKey: "userId",
-});
-
 // Routes
 app.use("/users", UserRouter);
-app.use("/accommodations", AccommodationRouter); 
+app.use("/accommodations", AccommodationRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${PORT}`);
